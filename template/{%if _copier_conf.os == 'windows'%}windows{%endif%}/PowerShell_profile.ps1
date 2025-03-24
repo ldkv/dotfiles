934aa025b9b6ab092f0e144e83cccc01094584bf
Vu LE <daokhanhvu.le@veesion.com>
2025-03-24 22:35:47 +0100

    New alias to open current repo in browser

--- template/{%if _copier_conf.os == 'windows'%}windows{%endif%}/PowerShell_profile.ps1
+++ template/{%if _copier_conf.os == 'windows'%}windows{%endif%}/PowerShell_profile.ps1
@@ -1,24 +1,34 @@
-# Environment Variables
+## Environment Variables
 $env:DEV = "D:\dev"
 $env:UV_CACHE_DIR = "$env:DEV\.config\uv\cache"
 $env:DOTFILES_PATH = "REPLACE_DOTFILES_PATH_HERE"
 $env:STARSHIP_CONFIG = "$env:DEV\.config\starship\starship.toml"
 
-# # Allow terminating the shell with Ctrl+D
+## Allow terminating the shell with Ctrl+D
 Set-PSReadlineKeyHandler -Chord Ctrl+d -Function DeleteCharOrExit
 
-# # uv shell completions - slow PowerShell startup
+## starship theme
+Invoke-Expression (&starship init powershell)
+
+## uv shell completions - slow PowerShell startup
 (& uv generate-shell-completion powershell) | Out-String | Invoke-Expression
 (& uvx --generate-shell-completion powershell) | Out-String | Invoke-Expression
 
-# # git aliases
+## git aliases
 Import-Module git-aliases -DisableNameChecking
-
-# starship theme
-Invoke-Expression (&starship init powershell)
 
 # dotfiles aliases
 function GotoDotfiles {cd $env:DOTFILES_PATH}
 function DotfilesUpdate {uvx copier update $env:DOTFILES_PATH --trust -A}
 Set-Alias dotfiles GotoDotfiles
 Set-Alias dotfiles-update DotfilesUpdate
+
+# Python venv - activate local venv
+function ActivateVenv {
+    if ($env:UV_PROJECT_ENVIRONMENT) {
+        . $env:UV_PROJECT_ENVIRONMENT/Scripts/Activate.ps1
+    } else {
+        . .venv/Scripts/Activate.ps1
+    }
+}
+Set-Alias venv ActivateVenv
